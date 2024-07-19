5a182812e925c4902341eaec73e905a2caf92bcc
Dibyas98 <dibyashaktip98@gmail.com>
2024-07-19 19:28:58 +0530

    stepper added

--- app/component/thinkofyou/ThinkOfYou.tsx
+++ app/component/thinkofyou/ThinkOfYou.tsx
@@ -1,6 +1,31 @@
 import React from 'react'
 import { MainBox } from '../ui/index'
+import { text } from 'stream/consumers'
+import { Span } from 'next/dist/trace'
+const stepper: { text: string, color: string,position?:string }[] = [
+    {
+        text: "You",
+        color: "6442EF",
+        position:'-top-11 -left-8'
+    },
+    {
+        text: "Anonymonos 1",
+        color: "40C2FC",
+        position:"top-8"
+    },
+    {
+        text: "Anonymonos 2",
+        color: "FDB338",
+        position:"-top-11"
+    },
+    {
+        text: "Anonymonos 3",
+        color: "59C798",
+        position:"top-8"
+    }
+]
 function ThinkOfYou(): React.ReactNode {
+   
     return (
         <MainBox>
             <main className='w-full lg:px-20 '>
@@ -12,21 +37,19 @@ function ThinkOfYou(): React.ReactNode {
                     <div className='w-full flex justify-center'>
                         <div className=' progres-shadow w-full md:w-3/4 lg:w-2/4 bg-white flex items-center md:h-[20vh] h-[30vh] rounded-xl'>
                             <div className=' w-full h-5 flex justify-between relative px-5'>
-                                <div className='w-[85%] md:w-[90%] lg:w-[92%] h-[1px] bg-gray-400 absolute top-[10px]'></div>
-                                <div className='w-5 h-5 rounded-full bg-blue-600 z-0 relative'>
-                                    <p className='absolute bg-blue-600 text-white p-2 px-4 rounded-md -top-14 -left-3'>
-                                        you
-                                    </p>
-                                </div>
-                                <div className='w-5 h-5 rounded-full bg-blue-600 z-0'>
+                                <div className='w-[90%] sm:w-[92%] md:w-[92%] lg:w-[90%] xl:w-[91%] h-[1px] bg-gray-400 absolute top-[10px]'></div>
+                                {
+                                    stepper.map((ele, idx) => {
+                                        return <div className={`w-5 h-5 rounded-full  z-0 relative text-wrap`} style={{backgroundColor:`#${ele.color}`}}>
+                                            <p className={`text-xs flex gap-1  absolute text-white py-2 px-4 rounded-md  ${ele.position}`} style={{backgroundColor:`#${ele.color}`}}>
+                                                {
+                                                    ele.text.split(" ").map((el) => <span>{el} </span>)
+                                                }
+                                            </p>
+                                        </div>
+                                    })
+                                }
 
-                                </div>
-                                <div className='w-5 h-5 rounded-full bg-blue-600 z-0'>
-
-                                </div>
-                                <div className='w-5 h-5 rounded-full bg-blue-600 z-0'>
-
-                                </div>
                             </div>
                         </div>
                     </div>
