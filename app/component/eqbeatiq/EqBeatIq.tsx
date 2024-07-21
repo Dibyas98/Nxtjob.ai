33586d03bc627175a8ddb15944d0c5725196a976
Dibyas98 <dibyashaktip98@gmail.com>
2024-07-22 00:13:18 +0530

    add img

--- app/component/eqbeatiq/EqBeatIq.tsx
+++ app/component/eqbeatiq/EqBeatIq.tsx
@@ -3,7 +3,7 @@ import { MainBox, Title, ParaCard } from '../ui/index'
 function EqBeatIq(): React.ReactNode {
     return (
         <MainBox>
-            <main className='w-full flex justify-center'>
+            <main className='w-full flex justify-center h-[50vh]'>
                 <section className='w-full md:w-3/4 lg:w-3/4 flex flex-col md:flex-row lg:flex-row justify-center gap-5 h-[40vh] md:items-center lg:items-center xl:items-center'>
                     <div className='w-full md:w-3/5 lg:w-1/4'>
                         <Title className='text-2xl font-bold text-center md:w-5/5 lg:w-4/5 lg:text-left md:text-left' title='EQ beats IQ'></Title>
