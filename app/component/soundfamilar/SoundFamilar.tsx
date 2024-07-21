94e5e7af7ec47c5fd6931a39f5651908b325fb17
Dibyas98 <dibyashaktip98@gmail.com>
2024-07-22 00:13:30 +0530

    add img

--- app/component/soundfamilar/SoundFamilar.tsx
+++ app/component/soundfamilar/SoundFamilar.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {MainBox,Card,Title} from '../ui/index'
+import Image from 'next/image'
 function SoundFamilar() {
-    const carddetails:{title:string,desc:string,emoji:string,back?:string}[]=[
+    const carddetails:{title:string,desc:string,emoji:string,back?:string,text?:string}[]=[
         {
             title:'You argue with a colleauge',
             desc:'You get angry and defensive, instead of staying open and working towards common ground.',
@@ -9,10 +10,11 @@ function SoundFamilar() {
             back:'#FFFFFF'
         },
         {
-            title:'You argue with a colleauge',
-            desc:'You get angry and defensive, instead of staying open and working towards common ground.',
-            emoji:'😤',
-            back:'#EEEBFE'
+            title:'You got s promotion at work',
+            desc:`Yor question yourself and wounder when they'll realize you're an unqualified imposter, instead of trusting youself & your abilities`,
+            emoji:'😲',
+            back:'#6341EF',
+            text:'white'
         },
         {
             title:'You argue with a colleauge',
@@ -36,12 +38,15 @@ function SoundFamilar() {
     <MainBox>
         <main className='w-full flex justify-center h-[80vh] items-center'>
             <section className='w-full xl:w-4/5 lg:w-4/5 lg:px-10 gap-5 flex flex-col'>
-                <Title title='Does this sound familiar' className='text-3xl font-bold lg:pl-28'></Title>
+                <div className='w-full flex items-center gap-4'>
+                <Title title='Does this sound familiar...' className='text-3xl font-bold lg:pl-28 w-fit'></Title>
+                <Image src='/brown-ghost.png' alt='img5' width={80} height={80}></Image>
+                </div>
                 <div className='w-100vh overflow-hidden soundfamilar'>
                 <div className='w-full flex overflow-x-scroll gap-10 lg:px-20 scrollbar-hide h-[400px] items-center'>
                     {
                         carddetails.map((ele,idx)=>{
-                            return <div className={`min-w-[250px] lg:w-full h-fit flex justify-end rounded-lg hover:scale-125 hover:-rotate-[25deg]`} style={{backgroundColor:ele.back,transitionDelay:'5sec'}} key={ idx}>
+                            return <div className={`min-w-[250px] lg:w-full h-fit flex justify-end rounded-lg hover:scale-125 hover:-rotate-[25deg]`} style={{backgroundColor:ele.back,color:ele.text?ele.text:'black'}} key={ idx}>
                             <Card
                                 title={ele.title}
                                 desc={ele.desc}
--- app/component/ui/Ball.tsx
+++ app/component/ui/Ball.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Ball() {
     return (
         <div className='w-1/2 h-full  relative overflow-hidden'>
-            <div className='w-36 h-36 md:w-56 md:h-56 lg:w-56 lg:h-56 bg-[#FAE8DC] rounded-full absolute top-36 -left-10 md:top-20 md:-left-16 lg:top-20 lg:-left-16 '>
+            <div className='w-52 h-52 md:w-56 md:h-56 lg:w-56 lg:h-56 bg-[#FAE8DC] rounded-full absolute top-36 -left-10 md:top-20 md:-left-16 lg:top-20 lg:-left-16 '>
                 <div className='w-full h-full relative '>
-                    <div className='w-16 h-16 md:w-36 md:h-36 lg:w-36 lg:h-36 absolute bg-white -right-5 md:-right-20 md:-top-8 lg:-right-20 lg:-top-8 rounded-full border-4 md:border-[15px] lg:border-[15px] border-[#FEF7F1]'>
-
+                    <div className='w-28 h-28 md:w-36 md:h-36 lg:w-36 lg:h-36 absolute bg-white -right-5 md:-right-20 md:-top-8 lg:-right-20 lg:-top-8 rounded-full border-4 md:border-[15px] lg:border-[15px] border-[#FEF7F1] flex justify-center items-center'>
+                        <Image src='/purpel-ghost.JPG' alt='img-3' width={50} height={50}></Image>
                     </div>
                 </div>
             </div>
